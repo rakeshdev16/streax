65ad749a0c288b759a443f7345349e4772ca75e9
Harshita Rawat <harshita.rawat@vinnisoft.org>
2024-10-04 20:54:53 +0530

    initial

---
+++ ios/Runner/Configuration.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum Configuration {
+    static let lensGroupId = "dfc17eb3-faa2-40ff-8f3c-751a3ae1218f"
+    static let applicationId = "1f58b025-0929-435a-a4b3-80937f7ca387"
+    static let apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjc4Mzc5MzkyLCJzdWIiOiIxZjU4YjAyNS0wOTI5LTQzNWEtYTRiMy04MDkzN2Y3Y2EzODd-U1RBR0lOR35jNzgzMGRhOC0yODZhLTRmZTktOTgyMS1lM2FjMWU1NjBlYzkifQ.o_EkiCigG9itTM48kDrhGb3mO7I1AVdXtbjTVcSYzHY"
+}
+
